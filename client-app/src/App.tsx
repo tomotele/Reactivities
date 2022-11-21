218831bdb2695ad561dd0c504b04ed4bf004745d
Tosin Olawale Tomotele {N} <tomotelet@accessbankplc.com>
2022-11-21 13:42:14 +0100

    added the client side

---
+++ client-app/src/App.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import { Header, List } from "semantic-ui-react";
+
+function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://localhost:5001/api/activities").then((response) => {
+      setActivities(response.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <Header as="h2" icon="users" content="Reactivities" />
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </>
+  );
+}
+
+export default App;
